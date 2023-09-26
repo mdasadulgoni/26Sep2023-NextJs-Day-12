999972d4ade51634c94e8d4cabc0c73e93471e79
mdasadulgoni <mdasadulgoni@gmail.com>
2023-09-26 23:39:07 +0530

    c5

--- src/app/food/page.js
+++ src/app/food/page.js
@@ -5,7 +5,7 @@ import React from 'react/no-unescaped-entities'
  function page() {
   return (
     <>
-
+          you&apos;re free
           <section className='col-lg-6 col-md-6 col-sm-6 mt-3'>
             <h1>The Food</h1>
             <p>Food is any substance consumed by an organism for nutritional support. Food is usually of plant, animal, or fungal origin and contains essential nutrients such as carbohydrates, fats, proteins, vitamins, or minerals. The substance is ingested by an organism and assimilated by the organism's cells to provide energy, maintain life, or stimulate growth. Different species of animals have different feeding behaviours that satisfy the needs of their metabolisms and have evolved to fill a specific ecological niche within specific geographical contexts.</p>
